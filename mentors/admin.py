0c96ee07718ff14daf61a87f8a9b9cb3ff9e5a9e
Kye Russell <me@kyerussell.com>
2014-06-29 19:00:19 +0800

    will fix later

--- mentors/admin.py
+++ mentors/admin.py
@@ -2,17 +2,17 @@ from django.contrib import admin
 from mentors.models import Mentor
 
 class MentorAdmin(admin.ModelAdmin):
-	list_display = ('name', 'shirt_size', 'wwcc_status', 'first_aid')
+	list_display = ('name', )
 
-	list_filter = ('first_aid', 'roles_desired', 'shirt_size')
+	list_filter = ('roles_desired', 'shirt_size')
 
-	fieldsets = [
-		('Personal Information', {'fields': ['contact_number', 'shirt_size']}),
-		('Industry Information', {'fields': ['uni', 'student_number', 'industry']}),
-		('Qualifications', {'fields': ['wwcc', 'wwcc_receipt', 'first_aid']}),
-		('Knowledge and Experience', {'fields': ['coding_experience', 'children_experience', 'children_experience_freeform']}),
-		('Aim', {'fields': ['aim', 'roles_desired', 'referral']}),
-		('Administration', {'fields': ['user', 'curtin_status', 'curtin_id']}),
-	]
+	#fieldsets = [
+	#	('Personal Information', {'fields': ['contact_number', 'shirt_size']}),
+	#	('Industry Information', {'fields': ['uni', 'student_number', 'industry']}),
+	#	('Qualifications', {'fields': ['wwcc', 'wwcc_receipt', 'first_aid']}),
+	#	('Knowledge and Experience', {'fields': ['coding_experience', 'children_experience', 'children_experience_freeform']}),
+	#	('Aim', {'fields': ['aim', 'roles_desired', 'referral']}),
+	#	('Administration', {'fields': ['user', 'curtin_status', 'curtin_id']}),
+	#]
 
 admin.site.register(Mentor, MentorAdmin)
