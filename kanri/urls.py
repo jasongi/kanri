2979269d52a665ad48d15232957efbb79334a996
Kye Russell <me@kyerussell.com>
2014-06-30 17:36:28 +0800

    dashboard now accessible from index

--- kanri/urls.py
+++ kanri/urls.py
@@ -5,12 +5,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'osameru.views.home', name='home'),
+    url(r'^$', 'dashboard.views.index', name='dashboard'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^dashboard/', include('dashboard.urls', namespace = "dashboard")),
-    url(r'^mentors/', include('mentors.urls', namespace = "mentors")),
     url(r'^ninjas/', include('ninjas.urls', namespace = "ninjas")),
     url(r'^planner/', include('planner.urls', namespace = "planner")),
     url(r'^attendance/', include('attendance.urls', namespace = "attendance")),
     url(r'^accounts/', include('accounts.urls', namespace = "accounts")),
+    url(r'^mentors/', include('mentors.urls', namespace = "mentors")),
 )
