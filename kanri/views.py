336a2bb10c413880368e083b10b8ed5dff689938
Kye Russell <me@kyerussell.com>
2014-07-01 20:14:37 +0800

    made generic list

--- kanri/views.py
+++ kanri/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 class KanriCreateView(CreateView):
 	template_name = 'generic_form.html'
@@ -9,3 +10,6 @@ class KanriUpdateView(UpdateView):
 
 class KanriDetailView(DetailView):
 	template_name = 'generic_detail.html'
+
+class KanriListView(ListView):
+	template_name = 'generic_list.html'
