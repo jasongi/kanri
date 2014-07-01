bc8d71a925013aa517bfc4fba78863c1b40b7f74
Kye Russell <me@kyerussell.com>
2014-07-01 12:17:20 +0800

    generic-ish detail view

--- kanri/views.py
+++ kanri/views.py
@@ -1,7 +1,11 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 
 class KanriCreateView(CreateView):
 	template_name = 'generic_form.html'
 
 class KanriUpdateView(UpdateView):
 	template_name = 'generic_form.html'
+
+class KanriDetailView(DetailView):
+	template_name = 'generic_detail.html'
