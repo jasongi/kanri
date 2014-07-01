ca61a7e8f28a7cff86301a0b574df55b62e7151f
Kye Russell <me@kyerussell.com>
2014-07-01 14:25:50 +0800

    added nicebool support to display_pair

--- kanri_template_tags/templatetags/display_pair.py
+++ kanri_template_tags/templatetags/display_pair.py
@@ -1,6 +1,8 @@
 from django import template
 from django.utils.safestring import mark_safe
 from django.utils.html import escape
+from kanri_template_tags.templatetags.nicebool import nicebool
+from django.template.defaultfilters import default
 
 register = template.Library()
 
@@ -9,13 +11,17 @@ def display_pair(field, name, autoescape = None):
 	if autoescape is not True:
 		raise ValueError("Auto escaping must be turned off for this filter to work")
 
+	if field is True or field is False:
+		field = nicebool(field, autoescape = True)
+	else:
+		field = escape(default(field, "-"))
 	markup = """
 	<div class="form-group">
 		<label class="col-sm-2 control-label">%s</label>
 		<div class="col-sm-10">
 			<p class="form-control-static">%s</p>
 		</div>
-	</div> """ % (name, escape(field))
+	</div> """ % (name, field)
 
 	return mark_safe(markup)
 
