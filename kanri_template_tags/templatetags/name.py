3e3f1348eecf7890bc75177e14379508767442cb
Kye Russell <me@kyerussell.com>
2014-07-01 20:11:35 +0800

    pluralised name filters

--- kanri_template_tags/templatetags/name.py
+++ kanri_template_tags/templatetags/name.py
@@ -2,14 +2,17 @@ from django import template
 
 register = template.Library()
 
-def name(model):
-	return model.__name__
+def name(model, plural = False):
+	if plural:
+		return template.defaultfilters.title(model._meta.verbose_name_plural)
+	else:
+		return model._meta.verbose_name
 
-def model_name(view):
-	return name(view.model)
+def model_name(view, plural = False):
+	return name(view.model, plural)
 
-def class_name(model):
-	return name(model.__class__)
+def class_name(model, plural = False):
+	return name(model.__class__, plural)
 
 
 register.filter('name', name)
