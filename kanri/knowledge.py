6b66e0fb5196bb4cc32b5d836830613827d66fe5
Kye Russell <me@kyerussell.com>
2014-07-04 11:14:24 +0800

    centralised knowledge gauging info

---
+++ kanri/knowledge.py
@@ -0,0 +1,11 @@
+NOTHING = 'NO'
+BEGINNER = 'BE'
+INTERMEDIARY = 'IN'
+ADVANCED = 'AD'
+
+KNOWLEDGE_CHOICES = (
+	(NOTHING, 'Nothing'),
+    (BEGINNER, 'Beginner'),
+    (INTERMEDIARY, 'Intermediary'),
+    (ADVANCED, 'Advanced')
+)
