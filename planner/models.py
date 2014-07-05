a729946dfac4c6d2cfedfcc693df7c11c8d2143a
Kye Russell <me@kyerussell.com>
2014-07-05 22:07:04 +0800

    just learned that timefields were a thing, lol

--- planner/models.py
+++ planner/models.py
@@ -16,15 +16,23 @@ class DojoTerm(models.Model):
 
 class DojoSession(models.Model):
 	term = models.ForeignKey(DojoTerm, help_text = "Dojo Term")
-	date_time_start = models.DateTimeField(unique = True, help_text = "Start Time")
-	date_time_end = models.DateTimeField(unique = True, help_text = "End Time")
+	date = models.DateField(
+		blank = False,
+		help_text = "Date during which the session will take place."
+	)
+
+	start = models.TimeField(
+		blank = False,
+		help_text = "Start Time"
+	)
+
+	end = models.TimeField(
+		blank = False,
+		help_text = "End Time"
+	)
 
 	def coming_up(self):
 		return abs((self.date_time_start - timezone.now).total_seconds()) < (2 * 60 * 60) # 2 hours
 
 	def __unicode__(self):
-		return "Session for %s (%s from %s to %s)" % (self.term,
-			self.date_time_start.strftime("%d/%m/%y"), 
-			self.date_time_start.strftime("%I:%M %p"),
-			self.date_time_end.strftime("%I:%M %p"),
-		)
+		return "%s (%s)" % (self.date.strftime("%d/%m"), self.term)
