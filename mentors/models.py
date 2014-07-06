fadc200566fc0e4378261633f9517ca0511a3821
Kye Russell <me@kyerussell.com>
2014-07-06 10:11:41 +0800

    roles don't require a description

--- mentors/models.py
+++ mentors/models.py
@@ -8,7 +8,7 @@ class Role(models.Model):
     """A role somebody can take in a CoderDojo term."""
     name = models.CharField(max_length = 50, blank = False, help_text = "The name of the role.")
     short_name = models.CharField(max_length = 10, blank = False, help_text = "A short name for this role. Used in spreadsheets.")
-    description = models.TextField(max_length = 1024, blank = False, help_text = "The role's long description.")
+    description = models.TextField(max_length = 1024, blank = True, help_text = "The role's long description.")
 
     def get_absolute_url(self):
         return reverse('mentors:role-detail', args = [self.id])
