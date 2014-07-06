e4e8a7804a3aa371be031ef716c04c0a3dffb835
Kye Russell <me@kyerussell.com>
2014-07-06 14:16:01 +0800

    permissions checks for ninja pages

--- ninjas/urls.py
+++ ninjas/urls.py
@@ -1,34 +1,41 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required, permission_required
 from ninjas import views
 
 urlpatterns = patterns('',
 	url(r'^$',
-		views.index,
+        permission_required('ninjas.view_ninja')
+		(views.index),
 		name = 'index'
 	),
 	
     url(r'^list$',
-    	views.NinjaList.as_view(),
+        permission_required('ninjas.view_ninja')
+    	(views.NinjaList.as_view()),
     	name = 'list'
     ),
 
     url(r'^(?P<pk>\d+)$',
-    	views.NinjaDetail.as_view(),
+        permission_required('ninjas.view_ninja')
+    	(views.NinjaDetail.as_view()),
     	name = 'detail'
     ),
 
     url(r'^add$',
-    	views.NinjaCreate.as_view(),
+        permission_required('ninjas.add_ninja')
+    	(views.NinjaCreate.as_view()),
     	name = 'add'
     ),
 
     url(r'^update/(?P<pk>\d+)$',
-    	views.NinjaUpdate.as_view(),
+        permission_required('ninjas.upload_ninja')
+    	(views.NinjaUpdate.as_view()),
     	name = 'update'
     ),
 
     url(r'^upload$',
-    	views.upload,
+        permission_required('ninjas.add_ninja')
+    	(views.upload),
     	name = 'upload'
     ),
 )
