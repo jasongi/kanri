36faeb50d7fb3587fe74b1a7a0b13ab579abba55
Kye Russell <me@kyerussell.com>
2014-07-07 07:55:21 +0800

    added attendance/on-the-day signoff

--- attendance/urls.py
+++ attendance/urls.py
@@ -3,7 +3,23 @@ from django.conf.urls import patterns, url
 from attendance import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name = 'index'),
-    # url(r'^(?P<mentor_id>\d+)/$', views.detail, name = 'detail'),
-    url(r'^signoff/(?P<dojo_session_id>\d+)$', views.signoff, name = 'signoff'),
+    url(r'^$',
+    	views.index,
+    	name = 'index'
+    ),
+
+    url(r'^signoff/(?P<dojo_session_id>\d+)$',
+    	views.signoff,
+    	name = 'signoff'
+    ),
+
+    url(r'^signoff/here/(?P<session_id>\d+)/(?P<ninja_id>\d+)',
+    	views.here,
+    	name = 'here'
+    ),
+
+    url(r'^signoff/nothere/(?P<attendance_id>\d+)',
+    	views.nothere,
+    	name = 'nothere'
+    )
 )
--- attendance/views.py
+++ attendance/views.py
@@ -1,16 +1,65 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from planner.models import DojoSession, DojoTerm
-import django.utils.timezone
+from ninjas.models import Ninja
+from attendance.models import Attendance
+from django.utils import timezone
+from django.core.urlresolvers import reverse
 import datetime
 
 def index(request):
-	sessions = DojoSession.objects.all().order_by('date_time_start')[:5]
+	sessions = DojoSession.objects.filter(date__gt = timezone.now()).order_by('date')[:5]
 	return render(request, 'attendance/index.html', {
 		'sessions': sessions,
 		})
 
 def signoff(request, dojo_session_id):
-	dojo = DojoSession.objects.get(pk = dojo_session_id)
-	return render(request, 'attendance/signoff.html', {
-		'dojo': dojo
+	session = DojoSession.objects.get(pk = dojo_session_id)
+	ninja_vm = []
+
+	for ninja in Ninja.objects.all():
+		here = Attendance.objects.filter(ninja = ninja, session = session)
+		if here:
+			here = here[0]
+
+		ninja_vm.append({
+			'ninja': ninja,
+			'attending': session in ninja.availabilities.all(),
+			'here': here
 		})
+
+	return render(request, 'attendance/signoff.html', {
+		'session': session,
+		'object_list': ninja_vm,
+		'ninja_count': len(ninja_vm),
+		'expected_attendance': session.get_expected_attendance(),
+		'expected_attendance_percentage': session.get_expected_attendance_percentage(),
+		'attendance': session.get_attendance(),
+		'attendance_percentage': session.get_attendance_percentage(),
+		'attendance_gap': session.get_expected_attendance_percentage() - session.get_attendance_percentage(),
+	})
+
+def here(request, session_id, ninja_id):
+	if request.method == "POST":
+		ninja = get_object_or_404(Ninja, pk = ninja_id)
+		print session_id
+		session = get_object_or_404(DojoSession, pk = session_id)
+
+		a = Attendance(
+			ninja = ninja,
+			session = session,
+			added_by = request.user
+		)
+
+		a.save()
+
+	return redirect('attendance:signoff', dojo_session_id = session_id)
+
+def nothere(request, attendance_id):
+	if request.method == "POST":
+		attendance = get_object_or_404(Attendance, pk = attendance_id)
+		session_id = attendance.session_id
+		attendance.delete()
+
+		return redirect('attendance:signoff', dojo_session_id = session_id)
+
+	
