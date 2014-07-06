a1eed3644c58e8fe37296e642a1b89d269ee7b6c
Kye Russell <me@kyerussell.com>
2014-07-07 07:56:15 +0800

    stop circular dependencies

--- attendance/models.py
+++ attendance/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from ninjas.models import Ninja
 from django.contrib.auth.models import User
 from django.conf import settings
 
 class Attendance(models.Model):
-	ninja = models.ForeignKey(Ninja)
+	ninja = models.ForeignKey('ninjas.Ninja')
 	session = models.ForeignKey('planner.DojoSession')
 	time = models.DateTimeField(auto_now_add = True)
 	added_by = models.ForeignKey(settings.AUTH_USER_MODEL)
