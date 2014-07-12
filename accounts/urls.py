43f2edd71d9c486115eb86321e12c0b261bf85ad
Kye Russell <me@kyerussell.com>
2014-07-12 15:23:28 +0800

    can add and remove user from management

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,39 @@ from accounts import views
 
 urlpatterns = patterns('',
     #url(r'^$', views.index, name = 'index'),
-    url(r'^login/?$', views.login, name = 'login'),
-    url(r'^logout?/$', views.logout, name = 'logout'),
-    url(r'^permissions$', views.permissions, name = 'permissions'),
-    url(r'^cp$', views.cp, name = 'cp'),
-    # url(r'^(?P<mentor_id>\d+)/$', views.detail, name = 'detail'),
+    url(r'^login/?$',
+    	views.login,
+    	name = 'login'
+    ),
+    
+    url(r'^logout?/$',
+    	views.logout,
+    	name = 'logout'
+    ),
+    
+
+    url(r'^permissions$',
+    	views.permissions,
+    	name = 'permissions'
+    ),
+
+    url(r'^permissions/sync$',
+    	views.sync,
+    	name = 'permissions-sync'
+    ),
+
+    url(r'^permissions/management/add$',
+    	views.management_add,
+    	name = 'management-add',
+    ),
+
+    url(r'^permissions/management/remove$',
+    	views.management_remove,
+    	name = 'management-remove',
+    ),
+
+    url(r'^cp$',
+    	views.cp,
+    	name = 'cp'
+    ),
 )
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from forms import LoginForm, UserSelectionForm, ManagementSelectionForm
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
 
 # Create your views here.
 def login(request):
@@ -35,6 +36,26 @@ def permissions(request):
 		'add_management_form': add_management_form,
 		'remove_management_form': remove_management_form,
 		})
+
+def management_add(request):
+	if request.method == 'POST':
+		form = UserSelectionForm(request.POST)
+		if form.is_valid():
+			group = Group.objects.get_or_create(name = 'Management')[0]
+			form.cleaned_data['user'].groups.add(group)
+			return redirect('accounts:permissions')
+	else:
+		return redirect('accounts:permissions')
+
+def management_remove(request):
+	if request.method == 'POST':
+		form = ManagementSelectionForm(request.POST)
+		if form.is_valid():
+			group = Group.objects.get_or_create(name = 'Management')[0]
+			group.user_set.remove(form.cleaned_data['user'])
+			return redirect('accounts:permissions')
+	else:
+		return redirect('accounts:permissions')
 
 def cp(request):
 	return redirect('dashboard:index')
